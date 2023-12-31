b6b1d4f1094055b95563b3dae048aad0cac5b636
farizalemuda <farizalemuda@gmail.com>
2023-12-31 16:51:14 +0700

    Update webserver-json.ino

--- webserver-json/webserver-json.ino
+++ webserver-json/webserver-json.ino
@@ -25,8 +25,6 @@ const char* password = "hihihihi";
 
 const char* PARAM_MESSAGE = "message";
 
-
-
 void notFound(AsyncWebServerRequest *request) {
     request->send(404, "text/plain", "Not found");
 }
