a04cc3af4bb0f748247e9a4440ac2638512843e3
farizalemuda <farizalemuda@gmail.com>
2023-12-31 19:55:06 +0700

    Create slave-rgb-lora-p2p-receiver.ino

---
+++ slave-rgb-lora-p2p-receiver/slave-rgb-lora-p2p-receiver.ino
@@ -0,0 +1,91 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://github.com/cosmic-id/
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+//define the pins used by the transceiver module
+#define LORA_AURORA_V2_NSS  15
+#define LORA_AURORA_V2_RST  0
+#define LORA_AURORA_V2_DIO0 27
+#define LORA_AURORA_V2_EN   32
+#define PIN_NEOPIXEL        33
+#define NUM_NEOPIXEL        1
+
+Adafruit_NeoPixel pixels(NUM_NEOPIXEL, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // Initiate the LoRa Enable pin
+  pinMode(LORA_AURORA_V2_EN, OUTPUT);
+  // LoRa chip is Active High
+  digitalWrite(LORA_AURORA_V2_EN, HIGH);
+  
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(LORA_AURORA_V2_NSS, LORA_AURORA_V2_RST, LORA_AURORA_V2_DIO0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(920E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+  #endif
+  // END of Trinket-specific code.
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      String LoRaData = LoRa.readString();
+      Serial.print(LoRaData); 
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+
+    pixels.clear(); // Set all pixel colors to 'off'
+
+    // The first NeoPixel in a strand is #0, second is 1, all the way up
+    // to the count of pixels minus one.
+    for(int i=0; i<NUM_NEOPIXEL; i++) { // For each pixel...
+
+      // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+      // Here we're using a moderately bright green color:
+      pixels.setPixelColor(i, pixels.Color(random(0,255), random(0,255), random(0,255)));
+
+      pixels.show();   // Send the updated pixel colors to the hardware.
+
+      delay(100); // Pause before next pass through loop
+    }
+  }
+}
